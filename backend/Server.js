a2ec697f6c014cd03e2a582f11c0929dd1a517d4
Lake Jasper <lakejasper@gmail.com>
2022-01-24 13:22:02 -0800

    Added Backend /contact end point Email Forwarding. Added .ignore to hide credentials

--- backend/Server.js
+++ backend/Server.js
@@ -1,13 +1,49 @@
+const { Credentials } = require ('./Credentials.js');
 const express = require('express');
-const cors=require("cors");
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3001;
 
 const corsOptions ={
-   origin:'*', 
-   credentials:true,
-   optionSuccessStatus:200,
+    origin:'*', 
+    credentials:true,
+    optionSuccessStatus:200,
+}
+
+const oAuth2Client = new google.auth.OAuth2(Credentials.CLIENT_ID, Credentials.CLIENT_SECRET, Credentials.REDIRECT_URI);
+oAuth2Client.setCredentials({refresh_token: Credentials.REFRESH_TOKEN})
+
+async function sendMail(body) {
+    try {
+        const accessToken = await oAuth2Client.getAccessToken();
+        const transport = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                type: 'oAuth2',
+                user: Credentials.User,
+                clientId: Credentials.CLIENT_ID,
+                clientSecret: Credentials.CLIENT_SECRET,
+                refreshToken: Credentials.REFRESH_TOKEN,
+                accessToken: accessToken
+            }
+        });
+
+        const mailOptions = {
+            from: Credentials.From,
+            to: Credentials.To,
+            subject: body.name + ' has made contact',
+            text: body.email + '\n' + body.message + '\n' + body.name + '\n' + body.phone + '\n' + body.company,
+            html:'<h1>' + body.email + '<br>' + body.message + '<br>' + body.name + '<br>' + body.phone + '<br>' + body.company + '</h1>'
+        }
+
+        const result = await transport.sendMail(mailOptions);
+        return result;
+    } catch(err) {
+        return err;
+    }
 }
 
 app.use(cors(corsOptions))
@@ -15,6 +51,7 @@ app.use(bodyParser.json());
 
 app.post("/Contact", (req, res) => {
     console.log(req.body);
+    sendMail(req.body).then((result) => console.log('Email Send', result)).catch((err) => console.log(error.message));
     return res.status(200).json({ success: true });
 });
 
